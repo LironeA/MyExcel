93206ad90996433ef4971c7fa13cb82b4769d29c
LironeA <322004@ukr.net>
2022-10-20 18:52:36 +0300

    Spreadsheet

---
+++ MyExcel/Spreadsheet/Cell.cs
@@ -0,0 +1,13 @@
+﻿using Parser;
+
+namespace MyExel.Spreadsheet
+{
+    class Cell
+    {
+        public Point index;
+        public string value;
+        public Formula formula;
+
+
+    }
+}
---
+++ MyExcel/Spreadsheet/SpreadSheet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Parser;
+
+namespace MyExel.Spreadsheet
+{
+    static class SpreadSheet
+    {
+        public static DataGridView GridView { get; set; }
+        private static Cell[,] cells = new Cell[100,100];
+        public static void CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+            cells[e.ColumnIndex, e.RowIndex] = new Cell();
+            cells[e.ColumnIndex, e.RowIndex].formula = new Formula((string)dgv.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+        }
+    }
+}
